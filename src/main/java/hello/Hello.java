856e8cfc690a8e18a96e428d64a270915117086b
Thalha uzair <thalhauzair786@gmail.com>
2020-02-11 15:24:29 +0530

    Test

---
+++ src/main/java/hello/Hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Hello {
+  String say()
+  {
+	 return "hello";
+  }
+  public static void main(String [] args)
+  {
+	  Hello a= new Hello();
+	  System.out.println(a.say());
+  }
+}
